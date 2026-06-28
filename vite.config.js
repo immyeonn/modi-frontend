d9db179bd9382082c8f7dbedbd42189d93c97769
immyeonn <kimsy060401@gmail.com>
2026-06-29 02:20:01 +0900

    chore: 폴더 구조 정리 및 alias 설정

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,22 @@
+// vite.config.js
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@styles": path.resolve(__dirname, "src/styles"), // ← 추가
+      "@components": path.resolve(__dirname, "src/components"),
+      "@router": path.resolve(__dirname, "src/router"),
+      "@store": path.resolve(__dirname, "src/store"),
+      "@api": path.resolve(__dirname, "src/api"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+    },
+  },
   server: {
     proxy: {
       "/api": {
